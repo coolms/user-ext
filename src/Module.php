dd80980a6e1d68f07b61eb36bd5a306a97c1e3ba
Dmitry Popov <d.popov@altgraphic.com>
2015-10-08 14:11:44 +0300

    Initial commit

--- src/Module.php
+++ src/Module.php
@@ -10,7 +10,9 @@
 
 namespace CmsUserExt;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface,
+use Zend\Loader\ClassMapAutoloader,
+    Zend\Loader\StandardAutoloader,
+    Zend\ModuleManager\Feature\AutoloaderProviderInterface,
     Zend\ModuleManager\Feature\ConfigProviderInterface,
     Zend\ModuleManager\ModuleManager;
 
@@ -32,10 +34,10 @@ class Module implements
     public function getAutoloaderConfig()
     {
         return [
-            'Zend\Loader\ClassMapAutoloader' => [
+            ClassMapAutoloader::class => [
                 __DIR__ . '/../autoload_classmap.php',
             ],
-            'Zend\Loader\StandardAutoloader' => [
+            StandardAutoloader::class => [
                 'fallback_autoloader' => true,
                 'namespaces' => [
                     __NAMESPACE__ => __DIR__,
